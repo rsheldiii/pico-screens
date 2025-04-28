1d0901c0112370ea2fc4eabb1b057e59f0c68a46
rsheldiii <rsheldiii@gmail.com>
2025-04-27 22:59:13 -0400

    Use constants from shared.h instead of redefining them

--- downsampler.c
+++ downsampler.c
@@ -1,40 +1,27 @@
 #include "pico.h"
+#include "screens/shared.h"
 // Remove the dependency on i_video.h
 // #include "i_video.h"
 
-// Define the necessary constants directly in this file
-#ifndef SCREENWIDTH
-#define SCREENWIDTH 256
-#endif
+// Use shared.h constants instead of redefining them
+// SCREENWIDTH, SCREENHEIGHT, DOWNSAMPLED_WIDTH and DOWNSAMPLED_HEIGHT are all defined there
 
-#ifndef SCREENHEIGHT
-#define SCREENHEIGHT 240
-#endif
-
-#ifndef DOWNSAMPLING_FACTOR
-#define DOWNSAMPLING_FACTOR 2.0
-#endif
-
-// Define these as macros instead of variable constants, and cast to integer types
-#define DOWNSAMPLED_WIDTH ((uint8_t)(SCREENWIDTH / DOWNSAMPLING_FACTOR))
-#define DOWNSAMPLED_HEIGHT ((uint8_t)(SCREENHEIGHT / DOWNSAMPLING_FACTOR))
-
-// Use a fixed-size buffer based on the expected result of DOWNSAMPLED_WIDTH
-// For SCREENWIDTH=256 and DOWNSAMPLING_FACTOR=2.0, this would be 128
-#define BUFFER_SIZE 128
-static uint16_t buffer[BUFFER_SIZE];
+// Buffer for processing a scanline
+static uint16_t buffer[DOWNSAMPLED_WIDTH];
 
 void downsample_scanline(uint16_t *src, uint16_t *dest) {
     for (uint8_t x = 0; x < DOWNSAMPLED_WIDTH; x++) {
       // Calculate the range of source pixels to average for this destination pixel
-      uint8_t start = (uint8_t)(x * DOWNSAMPLING_FACTOR);
-      uint8_t end = (uint8_t)((x + 1) * DOWNSAMPLING_FACTOR);
-      // we don't just use DOWNSAMPLING_FACTOR for this because it might be like 2.6 and right now we only do integer downsampling
+      // Using integer-based scaling as in shared.h
+      uint16_t start = (x * DOWNSAMPLING_FACTOR_OUT_OF_100) / 100;
+      uint16_t end = ((x + 1) * DOWNSAMPLING_FACTOR_OUT_OF_100) / 100;
       uint8_t total = end - start;
+      
+      if (total == 0) total = 1; // Avoid division by zero
 
       // Accumulate the sum of the source pixels in this range
       uint32_t r_sum = 0, g_sum = 0, b_sum = 0;
-      for (uint8_t current_pixel = start; current_pixel < end; current_pixel++) {
+      for (uint16_t current_pixel = start; current_pixel < end; current_pixel++) {
         r_sum += (src[current_pixel] & 0b1111100000000000) >> 11;
         g_sum += (src[current_pixel] & 0b0000011111100000) >> 5;
         b_sum += (src[current_pixel] & 0b0000000000011111);
