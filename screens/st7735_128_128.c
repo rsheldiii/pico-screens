5305b31c9718ea11fd63d9e49765b8ac2894b707
rsheldiii <rsheldiii@gmail.com>
2025-05-30 23:26:58 -0400

    remove some test code

--- screens/st7735_128_128.c
+++ screens/st7735_128_128.c
@@ -59,27 +59,27 @@ void st7735_128_128_initScreen(void) {
     // initialize 128 pixel row to blue
     uint16_t lcd_row[LCD_WIDTH];
 
-    // fill blue row with 0b0001111100000000
+    // // fill blue row with 0b0001111100000000
+    // for (uint8_t i = 0; i < LCD_WIDTH; i++) {
+    //     lcd_row[i] = 0b1111111111111111;
+    // }
+
+    // for(uint8_t y = 0; y < LCD_HEIGHT; y++) {
+    //     blit(0, y, LCD_WIDTH, 1, lcd_row);
+    // }
+
+    // sleep_ms(500);
+
+    // fill blue row with 0b0000000000000000
     for (uint8_t i = 0; i < LCD_WIDTH; i++) {
-        lcd_row[i] = 0b1111111111111111;
+        lcd_row[i] = 0b0000000000000000;
     }
 
     for(uint8_t y = 0; y < LCD_HEIGHT; y++) {
         blit(0, y, LCD_WIDTH, 1, lcd_row);
     }
 
-    sleep_ms(500);
-
-    // fill blue row with 0b0001111100000000
-    for (uint8_t i = 0; i < LCD_WIDTH; i++) {
-        lcd_row[i] = 0b0001111100000000;
-    }
-
-    for(uint8_t y = 0; y < LCD_HEIGHT; y++) {
-        blit(0, y, LCD_WIDTH, 1, lcd_row);
-    }
-
-    sleep_ms(500);
+    // sleep_ms(500);
 }
 
 void st7735_128_128_handleFrameStart(uint8_t frame) {
